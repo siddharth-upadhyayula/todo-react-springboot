c5189a7231ae6eb1a7f41d0cbdc44952329c03bd
Sai Siddharth Upadhyayula <usaisiddarth@gmail.com>
2024-01-26 16:32:21 -0500

    Modify Login

--- frontend/react-app/src/App.js
+++ frontend/react-app/src/App.js
@@ -1,10 +1,12 @@
 import TodoApp from './components/TodoApp';
+import LoginComponent from './components/LoginComponent'
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <TodoApp />
+      <LoginComponent />
     </div>
   );
 }
--- frontend/react-app/src/components/LoginComponent.jsx
+++ frontend/react-app/src/components/LoginComponent.jsx
@@ -1,8 +1,34 @@
-function LoginComponent() {
+import { useState } from "react"
+
+export default function LoginComponent() {
+    
+    const [username, setUsername] = useState()
+    const [password, setPassword] = useState()
+
+    function handleUsername(event) {
+        console.log(event.target.value)
+        setUsername(event.target.value)
+    }
+    
+    function handlePassword(event) {
+        console.log(event.target.value)
+        setPassword(event.target.value)
+    }
+
     return(
         <div className="Login">
-          <div className="LooginForm">
-            
+          <div className="LoginForm">
+            <div>
+                <label>User Name</label>
+                <input type="text" name="username" value={username} onChange={handleUsername}/>
+            </div>
+            <div>
+                <label>Password</label>
+                <input type="password" name="password" value={password} onChange={handlePassword}/>
+            </div>
+            <div>
+                <button type="button" name="login">login</button>
+            </div>
           </div>
         </div>
     )
